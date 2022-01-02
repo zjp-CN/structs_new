76f4b0b281956f62fc6c0e54ff03ea192718ab5f
zjp <jiping_zhou@foxmail.com>
2022-01-02 15:44:55 +0800

    remove the outdated comment

--- src/lib.rs
+++ src/lib.rs
@@ -2,12 +2,6 @@ use proc_macro::TokenStream;
 use quote::quote;
 use syn::parse_macro_input;
 
-/// struct D<T> where T: Copy {
-///     foo: u8,
-///     pub bar: T,
-///     abc: u8 = 255,
-/// }
-/// 暂时忽略字段前的属性
 mod structs;
 mod structs2;
 
